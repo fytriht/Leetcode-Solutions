f92f4c7cac8547cb01962380ed045c0b4d1e8e9c
fytriht <fytriht@gmail.com>
2016-11-06 10:12:14 +0800

    improved the algorihtm

--- Integer to Roman.js
+++ Integer to Roman.js
@@ -3,39 +3,61 @@
 /**
  * @param {number} num
  * @return {string}
+ *
+ * intToRoamn(2274) ==> MMCCLXXIV
  */
+
 var intToRoman = function(num) {
   
-  const pairs = [['1', 'I'], ['5', 'V'], ['10', 'X'],
-                  ['50', 'L'], ['100', 'C'], ['500', 'D'], ['1000', 'M']]
-  
-  const map = new Map(pairs)
+  var hash = [1,5,10,50,100,500,1000]
+  var vals = ['I','V','X','L','C','D','M']
 
-  const romanizer = (item, idx) => {
+  var arr = [], result = '', r
 
-    item = String(item)
-
-    if (map.has(item))
-      return map.get(item)
-    
-    if (item[0] == '9')
-      return romanizer(Math.pow(10, idx)) + 
-               romanizer(Number(item) + Math.pow(10, idx))
-  
-    if (item[0] <= '3')
-      return romanizer(Math.pow(10, idx)).repeat(item[0])
-    
-    if (item[0] == '4')
-      return romanizer(Math.pow(10, idx)) + 
-               romanizer(Number(item) + Math.pow(10, idx))
-    
-    return romanizer(item.replace(/\d/, '5')) + 
-             romanizer(Math.pow(10, idx)).repeat(item[0] - '5')
-
+  // if num=2273, result in arr=[3,7,2,2]
+  while (num) {
+    r = num % 10
+    arr.push(r)
+    num -= r
+    num /= 10
   }
 
-    // 1950 => [0, 50, 900, 1000]
-  return String(num).split('').reverse()
-           .map((item, idx) => item * Math.pow(10, idx))
-           .map(romanizer).reverse().join('')  
+  for (i = arr.length - 1; i >= 0; i--) {
+    result += digitToRoman(i, arr[i]) 
+  }
+
+  function digitToRoman(i, num) {
+    var result = '', d
+
+    if (num == 4) {
+      return digitToRoman(i, 1) + digitToRoman(i, 5)
+    }
+    if (num == 9) {
+      return digitToRoman(i, 1) + digitToRoman(i+1, 1)
+    }
+    
+    num *= Math.pow(10, i)
+    d = 2 * (i + 1) - 1  
+
+    if (hash[d] && num >= hash[d]) {
+      result += vals[d]
+      num -= hash[d]
+    }
+    d--
+    while (num) {
+      result += vals[d]
+      num -= hash[d]
+    }
+    return result
+  }
+  return result
 };
+
+// 3256 => MMMCCLVI 3000 + 200 + 50 + 6
+// 2234 => MMCCXXXIV 2000 + 200 + 30 + 4
+// 2274 => MMCCLXXIV
+// 2273 => MMCCLXXIII
+// 1996 => M CM XC VI 1000 + (1000 - 100) + (100 - 10) + 5 + 1
+
+// I V X   L   C   D   M
+// 1 5 10  50  100 500 1000
