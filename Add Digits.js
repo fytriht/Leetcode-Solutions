013106808326ef444f7c42cc90779d96bd6cace2
fytriht <fytriht@gmail.com>
2016-11-06 19:44:03 +0800

    added some comments

--- Add Digits.js
+++ Add Digits.js
@@ -3,6 +3,8 @@
 /**
  * @param {number} num
  * @return {number}
+ *
+ * if num = 38, so 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
  */
 var addDigits = function(num) {
     return num && ( num % 9 || 9 )
