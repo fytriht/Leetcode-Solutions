fef064ae365ace12c2bc340439807fc0b66f8d53
fytriht <fytriht@gmail.com>
2016-11-07 14:07:49 +0800

    updated

---
+++ Algorithms/Longest Common Prefix.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ *
+ * Given strs=['11','1111','122'], return '1'
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs.length) {
+    return ''
+  }
+  var prefix = strs[0], len
+
+  // find the string with shortest length.
+  for (var str of strs) {
+    if (str.length < prefix.lenght) {
+      prefix = str
+    }
+  }
+  for (var str of strs) {
+    prefix = findShortestPrefix(str, prefix)
+    if (!prefix.length) {
+      return ''
+    }
+  }
+  function findShortestPrefix(str, prefix) {
+    len = prefix.length  
+    prefix = prefix.split('')
+    while (len) {
+      if (prefix[len-1] != str[len-1]) {
+        prefix[len-1] = undefined // remove the last charater of `prefix`
+        prefix.length--
+      }
+      len--
+    }
+    return prefix.join('')
+  }
+  return prefix
+};
