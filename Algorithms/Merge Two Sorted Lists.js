a1b4fce7cb45984bcbe9591d9a71fa1f4ce0756c
fytriht <fytriht@gmail.com>
2016-11-10 14:51:30 +0800

    improved the algorithms

--- Algorithms/Merge Two Sorted Lists.js
+++ Algorithms/Merge Two Sorted Lists.js
@@ -1,45 +1,31 @@
 // https://leetcode.com/submissions/detail/81974486/
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-  var head, temp
 
-  // if l1=null, then return l2. vice versa
-  if (!l1 || !l2) 
-    return l1 || l2
-
-  // if l1.val > l2.val, switch l1 and l2
-  if (l1.val > l2.val) 
+ // if l1.val > l2.val, switch l1 and l2
+  if (l1 && l2 && l1.val > l2.val) 
     [l1, l2] = [l2, l1]
 
-  head = l1
-  while (l2) {
+  var head = l1, temp
+
+  while (l2 && l1) {
     if (!l1.next || l2.val < l1.next.val) {
-      temp = l2.next
-      l1 = insertNode(l1, l2)
-      l2 = temp
-    } else {
-      if (l1.next)
-        l1 = l1.next
+      l2 = insertNode(l1, l2)
     }
+    l1 = l1.next || l1
   }
   function insertNode(node1, node2) {
+    temp = node2.next
     node2.next = node1.next
     node1.next = node2
-    return node2
+    return temp
   }
-  return head
+  return head || l2
 };
 
 
