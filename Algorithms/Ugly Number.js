de8807bfd71135c11795e62115d0cb04b27912b5
Zhang Zhi <fytriht@gmail.com>
2016-11-26 12:53:16 +0800

    Added solutions

---
+++ Algorithms/Ugly Number.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/ugly-number/
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ * 
+ * Given num = 14, return false
+ */
+var isUgly = function(num) {
+  if (num < 1) {
+    return false
+  }
+  while (num > 5) {
+    if (num % 5 == 0) {
+      num /= 5
+      continue
+    }
+    if (num % 3 == 0) {
+      num /= 3
+      continue
+    }
+    if (num % 2 == 0) {
+      num /= 2
+      continue
+    }
+    return false
+  } 
+  return true
+};
+
+console.log(isUgly(15))
