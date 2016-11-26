645a508aad24b2b986fb32e42342e1316a72e2d6
Zhang Zhi <fytriht@gmail.com>
2016-11-26 12:57:53 +0800

    Improved the algorithms

--- Algorithms/Ugly Number.js
+++ Algorithms/Ugly Number.js
@@ -11,18 +11,30 @@ var isUgly = function(num) {
     return false
   }
   while (num > 5) {
-    if (num % 5 == 0) {
-      num /= 5
+    if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0) {
+      while (num % 5 == 0) {
+        num /= 5
+      }
+      while (num % 3 == 0) {
+        num /= 3
+      }
+      while (num % 2 == 0) {
+        num /= 2
+      }
       continue
     }
-    if (num % 3 == 0) {
-      num /= 3
-      continue
-    }
-    if (num % 2 == 0) {
-      num /= 2
-      continue
-    }
+    // if (num % 5 == 0) {
+    //   num /= 5
+    //   continue
+    // }
+    // if (num % 3 == 0) {
+    //   num /= 3
+    //   continue
+    // }
+    // if (num % 2 == 0) {
+    //   num /= 2
+    //   continue
+    // }
     return false
   } 
   return true
