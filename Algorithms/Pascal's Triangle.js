9756a0bfe5b0b05ed081f25de7711f99a1d0ce15
Zhang Zhi <fytriht@gmail.com>
2016-11-27 16:30:51 +0800

    improved the algorithms

--- Algorithms/Pascal's Triangle.js
+++ Algorithms/Pascal's Triangle.js
@@ -8,11 +8,14 @@ var generate = function(numRows) {
   if (!numRows) {
     return []
   }
-  var result = [[1]], i = 0
+  var result = [[1]], i = 0, j, temp
+
   function genRow(arr) {
     var row = [1]
-    for (i = 0; i < arr.length - 1; i++) {
-      row.push(arr[i] + arr[i+1])
+    for (j = 1; j < Math.round((arr.length + 1) / 2) ; j++) {
+      temp = arr[j-1] + arr[j]
+      row[j] = temp
+      row[arr.length - j] = temp
     }
     row.push(1)
     return row
@@ -36,12 +39,18 @@ console.log(generate(6))
 //    [1 ,5 ,10,10,5 ,1 ]
 // ]
 
-  // function genRow(arr) {
-  //   row = [1]
-  //   for (i = 0; i < arr.length - 1; i++) {
-  //     row.push(arr[i] + arr[i+1])
-  //   }
-  //   row.push(1)
-  //   return row
-  // }
-  // console.log(genRow([1]))
+
+
+// function genRow(arr) {
+//   var row = [1]
+//   var temp
+//   for (i = 1; i < Math.round((arr.length +1) / 2) ; i++) {
+//     temp = arr[i-1] + arr[i]
+//     row[i] = temp
+//     row[arr.length - i] = temp
+//   }
+//   row.push(1)
+//   return row
+// }
+
+// console.log(genRow([1,3,3,1]))
