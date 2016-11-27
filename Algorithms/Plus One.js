c1ab2cdac9371b45a8e0b29ff0f53c5cbec91a8e
Zhang Zhi <fytriht@gmail.com>
2016-11-27 15:00:42 +0800

    improved the algorithms

--- Algorithms/Plus One.js
+++ Algorithms/Plus One.js
@@ -6,21 +6,20 @@
  *
  * Given nums = [9,9], return [1,0,0]
  */
- var plusOne = function(digits) {
-   var i = digits.length - 1
-   digits[i]++
-   while (i > 0) {
-     if (digits[i] == 10) {
-       digits[i] = 0
-       digits[i-1]++
-     } else {
-       break
-     }
-     i--
-   }
-   if (digits[0] == 10) {
-     digits[0] = 1
-     digits[digits.length] = 0
-   }
-   return digits
- };
+var plusOne = function(digits) {
+  var i = digits.length - 1
+  digits[i]++
+  while (i > 0) {
+    if (digits[i] != 10) {
+      return digits
+    }
+    digits[i] = 0
+    digits[i-1]++      
+    i--
+  }
+  if (digits[0] == 10) {
+    digits[0] = 1
+    digits[digits.length] = 0
+  }
+  return digits
+};
