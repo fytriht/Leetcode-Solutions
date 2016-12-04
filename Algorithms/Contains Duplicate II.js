6dcb325ca29e75501c54ea9f6586bc0f253e1d5a
Zhang Zhi <fytriht@gmail.com>
2016-12-04 11:15:07 +0800

    added solutions

---
+++ Algorithms/Contains Duplicate II.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/submissions/detail/84636616/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ *
+ * Given nums = [1,0,1,1]  k = 1, return true 
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  var hash = {}, minDiff = Number.MAX_VALUE , i
+
+  for (i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] == undefined) {
+      hash[nums[i]] = i
+    } else {
+      if (i - hash[nums[i]] < minDiff) {
+        minDiff = i - hash[nums[i]]
+        hash[nums[i]] = i
+      }
+    }
+
+  }
+  return minDiff <= k
+};
+
+// console.log(containsNearbyDuplicate([1,0,1,1], 1))
