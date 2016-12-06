07eada4a347d3bbe96d56ceeecde8a73174ee201
Zhang Zhi <fytriht@gmail.com>
2016-12-06 21:11:59 +0800

    added solutions

---
+++ Algorithms/Intersection of Two Linked Lists.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  var hash = {}, nodeA = headA, nodeB = headB
+
+  while (nodeA || nodeB) {
+    if (nodeA) {
+      if (hash[nodeA.val]) {
+        return nodeA
+      }
+      hash[nodeA.val] = nodeA.val
+      nodeA = nodeA.next
+    }
+    if (nodeB) {
+      if (hash[nodeB.val]) {
+        return nodeB
+      }
+      hash[nodeB.val] = nodeB.val
+      nodeB = nodeB.next
+    }
+  }
+  return null
+};
