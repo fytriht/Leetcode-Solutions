d312b55ec118a880dacab318c619aaba0abfb592
Zhang Zhi <fytriht@gmail.com>
2016-12-10 17:21:30 +0800

    improved algorithms

--- Algorithms/ZigZag Conversion.js
+++ Algorithms/ZigZag Conversion.js
@@ -9,34 +9,32 @@ var convert = function(s, numRows) {
   if (numRows == 1)  return s
 
   var i = 0, 
+      j = 0,
       result = '',
-      step = 2 * (numRows - 1), // 8
-      len = s.length - 1,
-      row,
-      j
+      step = 2 * (numRows - 1),
+      len = s.length - 1        // 1 base length
 
   while (i < numRows) {
-    row = ''
-    j = 0
-
+    var row = ''
     while ((-i + step * (j + 1)) <= len || (i + step * j) <= len) {
-
       if ((i + step * j) <= len) {
-        // console.log((i + step * j), '!!')
         row += s[i + step * j] // 0 
       }
-      if ((i % (numRows - 1) != 0) && (-i + step * (j + 1)) <= len) {
+      if ((-i + step * (j + 1)) <= len && (i % (numRows - 1) != 0)) {
         row += s[-i + step * (j + 1)]
       }
       j++
     }
     result += row
     i++
+    j = 0
   }
   return result
 };
 
-// console.log(convert('PAYPALISHIRING', 2))
+ // beats 99.18% !
+
+console.log(convert('PAYPALISKJSDHKJDHSKDSHIRING', 2))
 
 // 0P 1A 2Y 3P 4A 5L 6I 7S 8H 9I 10R 11I 12N 13G
 
