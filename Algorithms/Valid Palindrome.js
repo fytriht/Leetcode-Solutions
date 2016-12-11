1889187311708132fe432f848ffeae938c575cdc
Zhang Zhi <fytriht@gmail.com>
2016-12-11 15:02:56 +0800

    improved the algorithms

--- Algorithms/Valid Palindrome.js
+++ Algorithms/Valid Palindrome.js
@@ -5,24 +5,20 @@
 //  * @return {boolean}
 //  */ 
 var isPalindrome = function(s) {
-  var 
-    i = 0, 
-    j = s.length -1,
-    r = /[A-Za-z0-9]/
+  var i = 0, j = s.length -1
 
   function isValid(m, n) {
-    if (m == n) {
-      return true
-    }
+    if (m == n) return true
+      
     return /[A-Za-z]/.test(m) && /[A-Za-z]/.test(n) &&
       Math.abs(m.charCodeAt(0) - n.charCodeAt(0)) == 32 
   }
 
   while (i < j) {
-    while (!r.test(s[i])) {
+    while (!/[A-Za-z0-9]/.test(s[i])) {
       i++
     }
-    while (!r.test(s[j])) {
+    while (!/[A-Za-z0-9]/.test(s[j])) {
       j--
     }
     if (i === j) break
@@ -33,4 +29,4 @@ var isPalindrome = function(s) {
   return true
 };
 
-console.log(isPalindrome("aA"))
+// console.log(isPalindrome("aA"))
