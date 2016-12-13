429be7cc3e60f8edb28845e6030d5640c28f5818
Zhang Zhi <fytriht@gmail.com>
2016-12-13 13:47:17 +0800

    improved the algorithms

--- Algorithms/Compare Version Numbers.js
+++ Algorithms/Compare Version Numbers.js
@@ -9,19 +9,19 @@ var compareVersion = function(version1, version2) {
   version2 = version2.split('.')
   version1 = version1.split('.')
 
-  var i = 0, len = Math.max(version1.length, version2.length), m, n
+  var 
+    i = 0, 
+    len = Math.max(version1.length, version2.length), 
+    m, 
+    n
 
   while (i < len) {
     if (version1[i] == undefined) {
-      while (version2[i] == 0) {
-        i++
-      }
+      while (version2[i] == 0) i++
       return version2[i] == undefined ? 0 : -1
     }
     if (version2[i] == undefined) {
-      while (version1[i] == 0) {
-        i++
-      }
+      while (version1[i] == 0) i++ 
       return version1[i] == undefined ? 0 : 1
     }
     m = Number(version1[i])
@@ -37,4 +37,4 @@ var compareVersion = function(version1, version2) {
   return 0
 };
 
-console.log(compareVersion("1.0.0", "1" ))
+console.log(compareVersion("01", "1" ))
