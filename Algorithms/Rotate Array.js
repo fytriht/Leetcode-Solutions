e56d5070269346fc90466b0063829158a02bc40f
Zhang Zhi <fytriht@gmail.com>
2016-12-13 13:29:36 +0800

    Organized the format

--- Algorithms/Rotate Array.js
+++ Algorithms/Rotate Array.js
@@ -2,12 +2,13 @@
 
 var rotate = function(nums, k) {
   if (k != 0) {
-    var arr = nums
-    arr = arr.slice(nums.length - k).concat(arr.slice(0, nums.length - k))
+    var arr = nums, i = nums.length - k
+    arr = arr.slice(i).concat(arr.slice(0, i))
     arr.forEach((num, i) => nums[i] = num)
   }
 };
 
 // [1,2,3,4,5,6,7]
+// [u,6,7,1,2,3,4]
 // [5,6,7,1,2,3,4]
 console.log(rotate([1,2], 1)) 
