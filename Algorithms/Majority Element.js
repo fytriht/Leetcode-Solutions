3c821681e1830f61b7eab4696f5c3b3873d7294f
Zhang Zhi <fytriht@gmail.com>
2016-12-16 11:19:01 +0800

    improved the algorithms

--- Algorithms/Majority Element.js
+++ Algorithms/Majority Element.js
@@ -8,14 +8,9 @@ var majorityElement = function(nums) {
   var hash = {} ,i = 0, len = nums.length
 
   while (i < len) {
-    if (hash[nums[i]] == undefined) {
-      hash[nums[i]] = 1
-    } else {
-      hash[nums[i]]++
-    }
-    if (hash[nums[i]] > len / 2) {
-      return nums[i]
-    }
+    if (hash[nums[i]] == undefined) hash[nums[i]] = 1
+    else hash[nums[i]]++
+    if (hash[nums[i]] > len / 2) return nums[i]
     i++  
   }
 };
