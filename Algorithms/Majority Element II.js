b3cdc27c8251144a7d4adf9ecc12fbd21af41174
Zhang Zhi <fytriht@gmail.com>
2016-12-16 13:51:01 +0800

    inproved the algorithms

--- Algorithms/Majority Element II.js
+++ Algorithms/Majority Element II.js
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/majority-element-ii/
+  // https://leetcode.com/problems/majority-element-ii/
 
 /**
  * @param {number[]} nums
@@ -12,19 +12,21 @@ var majorityElement = function(nums) {
     cond = len % 3 ? Math.ceil(len / 3) : len / 3 + 1
     i = 0
 
-  while (i < len) {
+  for (i = 0; i < len; i++) {
     if (hash[nums[i]] == undefined) {
       hash[nums[i]] = 1
     } else {
+      if (hash[nums[i]] == cond) {
+        continue
+      }
       hash[nums[i]]++
     }
     if (hash[nums[i]] == cond) {
       result.push(nums[i])
       if (result.length == 2) break
     }
-    i++
   }
   return result
-};
+}
 
-console.log(majorityElement([1]))
+  // console.log(majorityElement([1]))
