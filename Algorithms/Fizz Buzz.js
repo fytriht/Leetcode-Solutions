f5fe8e360f7d01cc9248072fb16b332c2cce84fc
Zhang Zhi <fytriht@gmail.com>
2016-12-18 17:13:06 +0800

    improve the algorithms

--- Algorithms/Fizz Buzz.js
+++ Algorithms/Fizz Buzz.js
@@ -1,25 +1,33 @@
 // https://leetcode.com/problems/fizz-buzz/
 
 /**
- * @param {string} s
- * @return {string}
- *
- * fizzBuzz(7) ==> [ '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7' ]
+ * @param {number} n
+ * @return {string[]}
  */
 var fizzBuzz = function(n) {
-  var arr = [], i = 0
-  function trans(num) {
-    if (num % 3 == 0 && num % 5 == 0) {
-      return 'FizzBuzz'
+  var result = [], i
+
+  for (i = 1; i <= n; i++) {
+    if (i % 3 == 0) {
+      
+      if (i % 5 == 0) {
+        result.push('FizzBuzz')
+      } else {
+        result.push('Fizz')  
+      }
+
+    } else {
+
+      if (i % 5 == 0) {
+        result.push('Buzz')
+      } else {
+        result.push(String(i))
+      }
+
     }
-    return (num % 3 == 0) ? 'Fizz' : 'Buzz'
   }
-  function isNeedTrans(num) {
-    return num % 3 == 0 || num % 5 == 0
-  }
-  while(i < n) {
-    arr[i] = isNeedTrans(i + 1) ? trans(i++ + 1) : String(i++ + 1)
-    arr[--n] = isNeedTrans(n + 1) ? trans(n + 1) : String(n + 1)
-  }
-  return arr
+  
+  return result
 };
+
+console.log(fizzBuzz(15))
