b039429df874bbe75f08d4466e0fa47b846abfbc
Zhang Zhi <fytriht@gmail.com>
2016-12-18 19:51:28 +0800

    improve the algorithms

--- Algorithms/Find the Difference.js
+++ Algorithms/Find the Difference.js
@@ -8,22 +8,36 @@
  * Given s='abcd' t='bacde', return 'e'
  */
 var findTheDifference = function(s, t) {
-  if (!s) return t
-  var i, len = s.length, cache = {}
+  var 
+    len = s.length, 
+    hash = {}, 
+    result,
+    i
+
   for (i = 0; i < len; i++) {
-    if (cache[s[i]] == undefined) 
-      cache[s[i]] = 0
-    if (cache[t[i]] == undefined)
-      cache[t[i]] = 0
-    cache[s[i]]++
-    cache[t[i]]--
+    if (hash[t[i]] == undefined) {
+      hash[t[i]] = 1
+    } else {
+      hash[t[i]]++
+    }
+   
+    if (hash[s[i]] == undefined) {
+      hash[s[i]] = -1
+    } else {
+      hash[s[i]]--
+    }
+  
   }
-  if (!cache[t[i]])
+  
+  if (!hash[t[i]]) {
     return t[i]
-  else
-    cache[t[i]]--
-  while (i--) {
-    if (cache[t[i]]) 
-      return t[i]
+  } else {
+    hash[t[i]]++
+  }
+  
+  for (var item in hash) {
+    if (hash[item] == 1) {
+      return item
+    }
   }
 };
