da5ac88125c82e8ee929e2a24a93fcfcff9c8b90
Zhang Zhi <fytriht@gmail.com>
2016-12-20 13:50:28 +0800

    improve the algorithms

--- Algorithms/Move Zeroes.js
+++ Algorithms/Move Zeroes.js
@@ -7,9 +7,10 @@
  * Given nums = [0, 1, 0, 3, 12], nums should be [1, 3, 12, 0, 0].
  */
 var moveZeroes = function(nums) {
-  var i = 0, 
-      j = 1, 
-      len = nums.length
+  var 
+    i = 0, 
+    j = 1, 
+    len = nums.length
   while (j < len) {
     while (nums[i] != 0 && i < len - 1) {
       i++
