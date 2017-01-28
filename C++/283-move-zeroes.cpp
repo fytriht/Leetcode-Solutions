454272d20e361f1571db96c223cf502fd977667c
Zhang Zhi <fytriht@gmail.com>
2017-01-28 20:00:03 +0800

    update

--- C++/283-move-zeroes.cpp
+++ C++/283-move-zeroes.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
 
   void moveZeroes(vector<int>& nums) {
-    int i = 0, j = 0, len = nums.size();
 
-    while (i < len) {
+    for (int i = 0, j = 0, size = nums.size(); i < size; i++) {
 
-      while (nums[i] == 0 && i < len - 1) {
-        i++;
-      }
+      while (nums[i] == 0 && i < size - 1) i++;
 
-      while (nums[j] != 0 && j < i) {
-        j++;
-      }
+      while (nums[j] != 0 && j < i) j++;
 
       swap(nums[i], nums[j]);
-      i++;
+      
     }
-
   }
+
 };
