a399552b9afa7e42d9e91cab1667ae5fa1021d59
Zhang Zhi <fytriht@gmail.com>
2017-02-03 14:39:06 +0800

    improve the algorithms

--- C++/383-ransom-note.cpp
+++ C++/383-ransom-note.cpp
@@ -3,18 +3,12 @@ public:
   bool canConstruct(string ransomNote, string magazine) {
     vector<int> h(26);
 
-    for (const auto& c : magazine) {
-      h[c - 'a']++;
-    }
+    for (const auto& c : magazine) h[c - 'a']++;
 
     for (const auto& c : ransomNote) {
-      h[c - 'a']--;
+      if (--h[c - 'a'] < 0) return false;
     }
 
-    for (const auto& c : h) {
-      if (c < 0) return false;
-    }
-    
     return true;
   }
 };
