3145d7440f03b58cabc4896eaa1b9f707f71b43d
Zhang Zhi <fytriht@gmail.com>
2017-06-09 15:07:37 +0800

    Add solutions

---
+++ Python/12-Integer-To-Roman.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        map = {
+            1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC",
+            50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+        }
+        result = ''
+        keys = sorted(map.keys())
+        for key in reversed(keys):
+            q = num // key
+            num -= q * key
+            if q > 0:
+                result += map[key] * q
+        return result
