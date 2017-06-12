cedae77519e5556e447bd11c37975c233f5981b7
Zhang Zhi <fytriht@gmail.com>
2017-06-12 13:39:48 +0800

    Improve the algorithms

--- Python/21-Merge-Two-Sorted-Lists.py
+++ Python/21-Merge-Two-Sorted-Lists.py
@@ -11,22 +11,8 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        head = ListNode(0)
-        curr = head
-
-        while l1 or l2:
-            if not l1:
-                while l2:
-                    curr.next = l2
-                    curr = curr.next
-                    l2 = l2.next
-                return head.next
-            if not l2:
-                while l1:
-                    curr.next = l1
-                    curr = curr.next
-                    l1 = l1.next
-                return head.next
+        curr = head = ListNode(0)
+        while l1 and l2:
             if l1.val <= l2.val:
                 curr.next = l1
                 l1 = l1.next
@@ -34,4 +20,5 @@ class Solution(object):
                 curr.next = l2
                 l2 = l2.next
             curr = curr.next
+        curr.next = l1 or l2
         return head.next
