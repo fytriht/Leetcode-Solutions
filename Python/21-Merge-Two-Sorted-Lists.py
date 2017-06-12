7a3bbe5655cb9371e45f129ada3e41700a429e18
Zhang Zhi <fytriht@gmail.com>
2017-06-12 13:34:46 +0800

    Improve the algori

--- Python/21-Merge-Two-Sorted-Lists.py
+++ Python/21-Merge-Two-Sorted-Lists.py
@@ -15,18 +15,18 @@ class Solution(object):
         curr = head
 
         while l1 or l2:
-            while not l1 and l2:
-                curr.next = l2
-                curr = curr.next
-                l2 = l2.next
-                if not l2:
-                    return head.next
-            while not l2 and l1:
-                curr.next = l1
-                curr = curr.next
-                l1 = l1.next
-                if not l1:
-                    return head.next
+            if not l1:
+                while l2:
+                    curr.next = l2
+                    curr = curr.next
+                    l2 = l2.next
+                return head.next
+            if not l2:
+                while l1:
+                    curr.next = l1
+                    curr = curr.next
+                    l1 = l1.next
+                return head.next
             if l1.val <= l2.val:
                 curr.next = l1
                 l1 = l1.next
