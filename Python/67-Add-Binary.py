c6e60c138ae609fac313a1cac8fb62d29071b755
Zhang Zhi <fytriht@gmail.com>
2017-06-17 14:48:45 +0800

    Add solutions

---
+++ Python/67-Add-Binary.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        carry = 0
+        result = ''
+        lengthA, lengthB = len(a), len(b)
+        for i in range(max(lengthA, lengthB)):
+            val = carry
+            if i < lengthA:
+                val += int(a[- (i + 1)])
+            if i < lengthB:
+                val += int(b[- (i + 1)])
+            result +=  str(val % 2)
+            carry = 1 if val >= 2 else 0
+        if carry == 1:
+            result += '1'
+        return result[::-1]
