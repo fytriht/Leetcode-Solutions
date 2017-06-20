77ed10173fadfd64c49d135c5248c9e9aa0f7b33
Zhang Zhi <fytriht@gmail.com>
2017-06-20 16:56:26 +0800

    Improve the algorithms

--- Python/136-Single-Number.py
+++ Python/136-Single-Number.py
@@ -4,13 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        cache = {}
-        for num in nums:
-            if num not in cache:
-                cache[num] = 1
-            else:
-                cache[num] += 1
-        for key, val in cache.items():
-            if val == 1:
-                return key
-        return None
+        result = nums[0]
+        for i in range(1, len(nums)):
+            result ^= nums[i]
+        return result
