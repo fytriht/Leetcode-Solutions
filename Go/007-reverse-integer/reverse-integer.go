1aeacfa885e33bd7f7fff56e53bd47554b9b5351
Zhang Zhi <fytriht@gmail.com>
2018-03-17 11:31:47 +0800

    Simplify

--- Go/007-reverse-integer/reverse-integer.go
+++ Go/007-reverse-integer/reverse-integer.go
@@ -5,8 +5,7 @@ import "math"
 func reverse(x int) int {
 	ret := 0
 	for x != 0 {
-		t := x % 10
-		ret = ret*10 + t
+		ret = ret*10 + x%10
 		x = x / 10
 	}
 	if ret < math.MinInt32 || ret > math.MaxInt32 {
