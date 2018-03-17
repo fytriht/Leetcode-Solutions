4e9377828b7857088033e2a9761c00178250ddae
Zhang Zhi <fytriht@gmail.com>
2018-03-17 12:20:13 +0800

    Add solutions

---
+++ Go/009-palindrome-number/palindrome_number.go
@@ -0,0 +1,21 @@
+package isPalindrome
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	bit := 1
+	for x/bit >= 10 {
+		bit = bit * 10
+	}
+	for x != 0 {
+		left := x / bit
+		right := x % 10
+		if left != right {
+			return false
+		}
+		x = (x - left*bit) / 10
+		bit = bit / 100
+	}
+	return true
+}
