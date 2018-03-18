3a5fd4408e19c6db02d2ed4cf99dd87b2467b520
Zhang Zhi <fytriht@gmail.com>
2018-03-18 17:21:42 +0800

    Refactor

--- Go/014-longest-common-prefix/longest_common_prefix.go
+++ Go/014-longest-common-prefix/longest_common_prefix.go
@@ -1,18 +1,16 @@
 package longestCommonPrefix
 
 func longestCommonPrefix(strs []string) string {
-	var ret string
 	if len(strs) == 0 {
-		return ret
+		return ""
 	}
-	for i, fst := 1, strs[0]; i <= len(fst); i++ {
-		tmp := fst[:i]
+	fst := strs[0]
+	for i := 1; i <= len(fst); i++ {
 		for j := 1; j < len(strs); j++ {
-			if len(strs[j]) < i || strs[j][:i] != tmp {
-				return ret
+			if len(strs[j]) < i || strs[j][:i] != fst[:i] {
+				return fst[:i-1]
 			}
 		}
-		ret = tmp
 	}
-	return ret
+	return fst
 }
