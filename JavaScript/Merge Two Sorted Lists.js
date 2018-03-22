3c8482058692b7d046ce422c45a7232ced592e93
Zhang Zhi <fytriht@gmail.com>
2018-03-22 22:29:07 +0800

    Simplify

--- JavaScript/Merge Two Sorted Lists.js
+++ JavaScript/Merge Two Sorted Lists.js
@@ -1,27 +1,25 @@
-let mergeTwoLists = (l1, l2) => {
-  if (!l1 || !l2) return l1 || l2
-
-  let head, curr
-  if (l1.val < l2.val) {
-    head = curr = l1
-    l1 = l1.next
-  } else {
-    head = curr = l2
-    l2 = l2.next
+function ListNode(val) {
+  if (!(this instanceof ListNode)) {
+    return new ListNode(val)
   }
+  this.val = val
+  this.next = null
+}
+
+let mergeTwoLists = (l1, l2) => {
+  let head = curr = ListNode()
 
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      curr.next = l1.next
+      curr.next = l1
       l1 = l1.next
     } else {
-      curr.next = l2.next
+      curr.next = l2
       l2 = l2.next
     }
     curr = curr.next
   }
 
   curr.next = l1 || l2
-
   return head.next
 }
