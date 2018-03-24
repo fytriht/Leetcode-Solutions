0d60d56f6de47951f1fdcafa5f048d6e72aed280
Zhang Zhi <fytriht@gmail.com>
2018-03-24 17:30:30 +0800

    Refactor

--- Go/35-search-insert-position/search-insert-position.go
+++ Go/35-search-insert-position/search-insert-position.go
@@ -1,10 +1,14 @@
 package searchInsert
 
 func searchInsert(nums []int, target int) int {
-	for i, n := range nums {
-		if n == target || n > target {
-			return i
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if target <= nums[m] {
+			r = m - 1
+		} else {
+			l = m + 1
 		}
 	}
-	return len(nums)
+	return l
 }
