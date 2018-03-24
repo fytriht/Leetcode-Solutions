718588b89f07a81ceb212a44743fe077c5826113
Zhang Zhi <fytriht@gmail.com>
2018-03-24 23:42:47 +0800

    Add solutions

---
+++ Go/038-count-and-say/count_and_say.go
@@ -0,0 +1,23 @@
+package countAndSay
+
+import "strconv"
+
+func countAndSay(n int) string {
+	if n == 1 {
+		return "1"
+	}
+	return getNext(countAndSay(n - 1))
+}
+
+func getNext(s string) string {
+	ret := ""
+	for i := 0; i < len(s); i++ {
+		count := 1
+		for i < len(s)-1 && s[i] == s[i+1] {
+			i++
+			count++
+		}
+		ret += strconv.Itoa(count) + s[i:i+1]
+	}
+	return ret
+}
