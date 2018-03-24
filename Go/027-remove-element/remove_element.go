4d5a095e33bd3bf19d9ae82d2aafe315430a3526
Zhang Zhi <fytriht@gmail.com>
2018-03-24 14:39:47 +0800

    Refactor

--- Go/027-remove-element/remove_element.go
+++ Go/027-remove-element/remove_element.go
@@ -3,11 +3,11 @@ package removeElement
 func removeElement(nums []int, val int) int {
 	i, j := 0, len(nums)
 	for i < j {
-		if nums[i] != val {
-			i++
-		} else {
+		if nums[i] == val {
 			j--
 			nums[i] = nums[j]
+		} else {
+			i++
 		}
 	}
 	return j
