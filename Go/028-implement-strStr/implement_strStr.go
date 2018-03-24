535ed8c51d72ac25d5998e2da1d533fab9925943
Zhang Zhi <fytriht@gmail.com>
2018-03-24 15:09:46 +0800

    Add todos

--- Go/028-implement-strStr/implement_strStr.go
+++ Go/028-implement-strStr/implement_strStr.go
@@ -1,5 +1,7 @@
 package strStr
 
+// TODO: KMP Algorithm http://en.wikipedia.org/wiki/Knuth-Morris-Pratt_algorithm
+
 func strStr(haystack string, needle string) int {
 	for i := 0; i <= len(haystack)-len(needle); i++ {
 		if haystack[i:i+len(needle)] == needle {
