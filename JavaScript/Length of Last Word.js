c962e88bbaec3524c838f9cea5bb1360ce270176
Zhang Zhi <fytriht@gmail.com>
2018-03-25 22:21:32 +0800

    Update solutions

--- JavaScript/Length of Last Word.js
+++ JavaScript/Length of Last Word.js
@@ -1,17 +1,11 @@
-// https://leetcode.com/problems/length-of-last-word/
-
-/**
- * @param {string} s
- * @return {number}
- * 
- * Given s = 'hello world', return 5
- */
-var lengthOfLastWord = function(s) {
-  if (!s) return 0
-  var i = s.length, counts = 0
-  // find the last index of not blank(' ') value
-  while (s[--i] === ' ');
-  // count the length of last word
-  while (i >= 0 && s[i--] !== ' ') counts++;
-  return counts
-};
+let lengthOfLastWord = s => {
+  let length = 0
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] == ' ') {
+      if (length > 0) break
+    } else {
+      length++
+    }
+  }
+  return length
+}
