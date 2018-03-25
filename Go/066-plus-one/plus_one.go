ece51bcedb3fdda1c7bd5793dd8911ab6570081b
Zhang Zhi <fytriht@gmail.com>
2018-03-25 22:49:34 +0800

    Simplify

--- Go/066-plus-one/plus_one.go
+++ Go/066-plus-one/plus_one.go
@@ -1,17 +1,13 @@
 package plusOne
 
 func plusOne(digits []int) []int {
-	carry := true
-	for i := len(digits) - 1; carry && i >= 0; i-- {
-		digits[i] = digits[i] + 1
-		if digits[i] == 10 {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
 			digits[i] = 0
 		} else {
-			carry = false
+			digits[i] += 1
+			return digits
 		}
 	}
-	if carry {
-		digits = append([]int{1}, digits...)
-	}
-	return digits
+	return append([]int{1}, digits...)
 }
