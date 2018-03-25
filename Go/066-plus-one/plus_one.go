f0178786671621dd4a3d9ccf6b034b1c1018a15f
Zhang Zhi <fytriht@gmail.com>
2018-03-25 23:00:20 +0800

    Refactor

--- Go/066-plus-one/plus_one.go
+++ Go/066-plus-one/plus_one.go
@@ -2,12 +2,11 @@ package plusOne
 
 func plusOne(digits []int) []int {
 	for i := len(digits) - 1; i >= 0; i-- {
-		if digits[i] == 9 {
-			digits[i] = 0
-		} else {
+		if digits[i] != 9 {
 			digits[i] += 1
 			return digits
 		}
+		digits[i] = 0
 	}
 	return append([]int{1}, digits...)
 }
