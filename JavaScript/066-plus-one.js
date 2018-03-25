3c8d4d0a2800b6b723032c6b840eb0ac71115b34
Zhang Zhi <fytriht@gmail.com>
2018-03-25 23:01:15 +0800

    Refactor

--- JavaScript/066-plus-one.js
+++ JavaScript/066-plus-one.js
@@ -1,11 +1,10 @@
 let plusOne = digits => {
   for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] == 9) {
-      digits[i] = 0
-    } else {
+    if (digits[i] != 9) {
       digits[i]++
       return digits
     }
+    digits[i] = 0
   }
   return [1, ...digits]
 }
