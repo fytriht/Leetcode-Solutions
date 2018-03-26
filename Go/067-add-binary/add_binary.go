00a6aafb14df2c70545cea102ec504fe0d9d3915
Zhang Zhi <fytriht@gmail.com>
2018-03-26 21:25:49 +0800

    Add solutions

---
+++ Go/067-add-binary/add_binary.go
@@ -0,0 +1,32 @@
+package addBinary
+
+import "strconv"
+
+func addBinary(a string, b string) string {
+	ret := ""
+	carry := 0
+	la, lb := len(a), len(b)
+	for i := 0; i < max(la, lb); i++ {
+		va, vb := 0, 0
+		if i < la {
+			va, _ = strconv.Atoi(a[la-i-1 : la-i])
+		}
+		if i < lb {
+			vb, _ = strconv.Atoi(b[lb-i-1 : lb-i])
+		}
+		tmp := carry + va + vb
+		carry = tmp / 2
+		ret = strconv.Itoa(tmp%2) + ret
+	}
+	if carry != 0 {
+		ret = strconv.Itoa(carry) + ret
+	}
+	return ret
+}
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
