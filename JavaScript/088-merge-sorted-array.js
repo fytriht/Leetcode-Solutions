5813858b78e44ec231a8d2bb86dcde979f6c7f6a
Zhang Zhi <fytriht@gmail.com>
2018-03-30 00:23:15 +0800

    Fancier

--- JavaScript/088-merge-sorted-array.js
+++ JavaScript/088-merge-sorted-array.js
@@ -1,7 +1,9 @@
 let merge = (nums1, m, nums2, n) => {
-  let i = m-- + n-- - 1
-  while (m >= 0 && n >= 0) {
+  let i = m + n
+  for (
+    m--, n--, i--;
+    m >= 0 && n >= 0;
     nums1[i--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--]
-  }
-  while (n >= 0) nums1[i--] = nums2[n--]
+  ) {}
+  for (; n >= 0; nums1[i--] = nums2[n--]) {}
 }
