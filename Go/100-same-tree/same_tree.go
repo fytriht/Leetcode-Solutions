d4e20762d4a4129631a84100aa8e33d1b77e82eb
Zhang Zhi <fytriht@gmail.com>
2018-03-31 15:33:17 +0800

    Add solutions

---
+++ Go/100-same-tree/same_tree.go
@@ -0,0 +1,16 @@
+package isSameTree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		return p == nil && q == nil
+	}
+	return p.Val == q.Val &&
+		isSameTree(p.Left, q.Left) &&
+		isSameTree(p.Right, q.Right)
+}
