4dee63bab516bd4601b4de6b2006ed563905681d
Zhang Zhi <fytriht@gmail.com>
2018-04-04 00:02:52 +0800

    Add solutions

---
+++ Go/110-balanced-binary-tree/balanced_binary_tree.go
@@ -0,0 +1,39 @@
+package isBalanced
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// TODO: more efficent
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if abs(treeDepth(root.Left)-treeDepth(root.Right)) > 1 {
+		return false
+	}
+	return isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func treeDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
