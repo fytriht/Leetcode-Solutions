3a4903c0b95daa860125698feb00fe51af0bc9a6
Zhang Zhi <fytriht@gmail.com>
2018-04-05 16:17:13 +0800

    Add solutions

---
+++ Go/119-pascals-triangle-II/pascals_triangle_II.go
@@ -0,0 +1,12 @@
+package getRow
+
+func getRow(rowIndex int) []int {
+	ret := make([]int, rowIndex+1)
+	ret[0] = 1
+	for i := 0; i < rowIndex; i++ {
+		for j := i + 1; j > 0; j-- {
+			ret[j] += ret[j-1]
+		}
+	}
+	return ret
+}
