919dcb4ad441daebea50afd12c48e06923a4e9fb
Zhang Zhi <fytriht@gmail.com>
2018-04-05 14:29:33 +0800

    Add solutions

---
+++ Go/118-pascals-triangle/pascals_triangle.go
@@ -0,0 +1,18 @@
+package generate
+
+func generate(numRows int) [][]int {
+	var ret [][]int
+	if numRows == 0 {
+		return ret
+	}
+	ret = append(ret, []int{1})
+	for i := 1; i < numRows; i++ {
+		curr := []int{1}
+		prev := ret[i-1]
+		for i := 0; i < len(prev)-1; i++ {
+			curr = append(curr, prev[i]+prev[i+1])
+		}
+		ret = append(ret, append(curr, 1))
+	}
+	return ret
+}
