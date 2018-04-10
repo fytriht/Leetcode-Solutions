a5245be623652c06a2869b49839250f790e17a95
Zhang Zhi <fytriht@gmail.com>
2018-04-10 22:26:07 +0800

    Simpify

--- Go/171-excel-sheet-column-number/excel_sheet_column_number.go
+++ Go/171-excel-sheet-column-number/excel_sheet_column_number.go
@@ -1,14 +1,9 @@
 package title
 
-import (
-	"math"
-)
-
 func titleToNumber(s string) int {
 	ret := 0
-	for i := len(s) - 1; i >= 0; i-- {
-		base := math.Pow(26, float64(len(s)-i-1))
-		ret += int(s[i]-'A'+1) * int(base)
+	for _, b := range s {
+		ret = ret*26 + int(b-'A'+1)
 	}
 	return ret
 }
