456ee4392b54ee3e23af6cdcc324ac84886b94d4
Zhang Zhi <fytriht@gmail.com>
2018-04-14 22:45:42 +0800

    Add solutions

---
+++ Go/203-remove-linked-list-elements/remove_linked_list_elements.go
@@ -0,0 +1,17 @@
+package remove
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+	head.Next = removeElements(head.Next, val)
+	if head.Val == val {
+		return head.Next
+	}
+	return head
+}
