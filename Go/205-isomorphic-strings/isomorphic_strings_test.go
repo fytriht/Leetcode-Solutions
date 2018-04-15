12b697185a211e87cbdb5bd2d9e3b4510930d736
Zhang Zhi <fytriht@gmail.com>
2018-04-15 21:56:17 +0800

    Add tests

---
+++ Go/205-isomorphic-strings/isomorphic_strings_test.go
@@ -0,0 +1,31 @@
+package isomorphic
+
+import (
+	"testing"
+)
+
+type testcase struct {
+	ina  string
+	inb  string
+	want bool
+}
+
+func TestIsIsomorphic(t *testing.T) {
+	cases := []testcase{
+		{"egg", "add", true},
+		{"foo", "bar", false},
+		{"paper", "title", true},
+		{"ab", "cc", false},
+	}
+	for _, c := range cases {
+		if got := isIsomorphic(c.ina, c.inb); got != c.want {
+			t.Errorf(
+				"isIsomorphic(\"%s\", \"%s\"): expected %t but got %t",
+				c.ina,
+				c.inb,
+				c.want,
+				got,
+			)
+		}
+	}
+}
