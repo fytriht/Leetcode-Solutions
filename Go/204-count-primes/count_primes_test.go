01c71e621baeed4792805cec66b31573910e756e
Zhang Zhi <fytriht@gmail.com>
2018-04-15 18:14:34 +0800

    Add test cases

---
+++ Go/204-count-primes/count_primes_test.go
@@ -0,0 +1,24 @@
+package count
+
+import (
+	"testing"
+)
+
+type testcase struct {
+	in   int
+	want int
+}
+
+func TestCountPrimies(t *testing.T) {
+	cases := []testcase{
+		{0, 0},
+		{1, 0},
+		{10, 4},
+		{499979, 41537},
+	}
+	for _, c := range cases {
+		if got := countPrimes(c.in); got != c.want {
+			t.Errorf("Expected '%d' but got '%d'", c.want, got)
+		}
+	}
+}
