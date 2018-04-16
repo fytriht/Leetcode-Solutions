1f1f3fc7cb049110422ebd642c5446e12eff7d10
Zhang Zhi <fytriht@gmail.com>
2018-04-16 21:46:52 +0800

    Add solutions

---
+++ Go/206-reverse-linked-list/reverse_linked_list.go
@@ -0,0 +1,16 @@
+package reverse
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	r := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return r
+}
