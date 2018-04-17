99bd2aca30ed7b804b774a69cd689104d5507061
Zhang Zhi <fytriht@gmail.com>
2018-04-17 22:01:17 +0800

    Add test cases

---
+++ Go/217-contains-duplicate/contains_duplicate_test.go
@@ -0,0 +1,30 @@
+package duplicate
+
+import (
+	"testing"
+)
+
+type testcase struct {
+	in   []int
+	want bool
+}
+
+func TestContainsDuplicate(t *testing.T) {
+	cases := []testcase{
+		{[]int{}, false},
+		{[]int{1}, false},
+		{[]int{1, 1}, true},
+		{[]int{1, 1, 1}, true},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, false},
+	}
+	for _, c := range cases {
+		if got := containsDuplicate(c.in); got != c.want {
+			t.Errorf(
+				"containsDuplicate(%v): expected %t but got %t",
+				c.in,
+				c.want,
+				got,
+			)
+		}
+	}
+}
