dcfe7ae781e5782e1d1d4ce551375fa973dd0904
Zhang Zhi <fytriht@gmail.com>
2018-04-18 20:56:24 +0800

    Add solutions

---
+++ Go/219-contains-duplicate-II/contains_duplicate_II.go
@@ -0,0 +1,12 @@
+package duplicate
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	m := map[int]int{}
+	for i, n := range nums {
+		if idx, ok := m[n]; ok && i-idx <= k {
+			return true
+		}
+		m[n] = i
+	}
+	return false
+}
