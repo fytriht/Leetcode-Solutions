76cdc4177d78b6522fe2b3ac7cdcfb1dd16a087e
Zhang Zhi <fytriht@gmail.com>
2018-04-18 22:21:05 +0800

    Add test cases

---
+++ Go/226-invert-binary-tree/invert_binary_tree_test.go
@@ -0,0 +1,16 @@
+package invert
+
+import (
+	"testing"
+)
+
+func TestInvertTree(t *testing.T) {
+	l := &TreeNode{Val: 1}
+	r := &TreeNode{Val: 2}
+	root := &TreeNode{Left: l, Right: r}
+
+	invertTree(root)
+	if root.Left.Val != r.Val || root.Right.Val != l.Val {
+		t.Error("expected the tree to be inverted")
+	}
+}
