1ec3bede9bb306e69d184cc861176d03b60da9bc
Zhang Zhi <fytriht@gmail.com>
2018-04-19 22:58:37 +0800

    Add test cases

---
+++ Go/232-implement-queue-using-stacks/implement_queue_using_stacks_test.go
@@ -0,0 +1,41 @@
+package queue
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMyQueue(t *testing.T) {
+	s := Constructor()
+	if !s.Empty() {
+		t.Errorf("expected to be empty")
+	}
+	s.Push(1)
+	if s.Empty() {
+		t.Errorf("expected push works")
+	}
+
+	s1 := Constructor()
+	s1.Push(1)
+	s1.Push(2)
+	if s1.Peek() != 1 {
+		t.Errorf("expected to get front element")
+	}
+
+	s2 := Constructor()
+	s2.Push(1)
+	s2.Pop()
+	if !s2.Empty() {
+		t.Errorf("expected pop works")
+	}
+
+	s3 := Constructor()
+	s3.Push(1)
+	s3.Push(2)
+	s3.Push(3)
+	fmt.Println(s3.s.data)
+	if s3.Peek() != 1 {
+		t.Error("expected to get front element")
+	}
+
+}
