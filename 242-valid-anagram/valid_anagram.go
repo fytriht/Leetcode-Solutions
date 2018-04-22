2b1c1fffec0c2729bb1165f8dfeb067635d10ca5
Zhang Zhi <fytriht@gmail.com>
2018-04-22 13:42:10 +0800

    Add solutions

---
+++ 242-valid-anagram/valid_anagram.go
@@ -0,0 +1,18 @@
+package solution
+
+func isAnagram(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	m := map[byte]int{}
+	for i := range s {
+		m[s[i]]++
+		m[t[i]]--
+	}
+	for _, v := range m {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
