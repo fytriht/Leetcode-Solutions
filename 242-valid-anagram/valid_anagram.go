7fee6acfd600ace859c7bad676abbc48034be6d6
Zhang Zhi <fytriht@gmail.com>
2018-04-22 13:59:26 +0800

    Add solutions

--- 242-valid-anagram/valid_anagram.go
+++ 242-valid-anagram/valid_anagram.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+	"sort"
+)
+
+//
+// solution 1
+//
+
 func isAnagram(s, t string) bool {
 	if len(s) != len(t) {
 		return false
@@ -15,4 +23,32 @@ func isAnagram(s, t string) bool {
 		}
 	}
 	return true
+}
+
+//
+// solution 2
+//
+
+func isAnagram2(s, t string) bool {
+	return SortString(s) == SortString(t)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRune(r))
+	return string(r)
+}
+
+type sortRune []rune
+
+func (s sortRune) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRune) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRune) Len() int {
+	return len(s)
 }
