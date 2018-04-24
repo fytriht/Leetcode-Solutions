e2441c731a9ee0b3e5a291ac14636fede4e7b36b
Zhang Zhi <fytriht@gmail.com>
2018-04-24 22:38:31 +0800

    Add test cases

---
+++ 263-ugly-number/ugly_number_test.go
@@ -0,0 +1,28 @@
+package solution
+
+import (
+	"testing"
+)
+
+func TestIsUgly(t *testing.T) {
+	cases := []struct {
+		in   int
+		want bool
+	}{
+		{1, true},
+		{6, true},
+		{8, true},
+		{14, false},
+	}
+
+	for _, c := range cases {
+		if got := isUgly(c.in); got != c.want {
+			t.Errorf(
+				"isUgly(%d): expected %t but got %t",
+				c.in,
+				c.want,
+				got,
+			)
+		}
+	}
+}
