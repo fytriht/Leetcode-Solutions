18cf79541e0cde51203c3275a3c55e2402f27d07
Zhang Zhi <fytriht@gmail.com>
2018-04-29 14:04:30 +0800

    Add test cases

---
+++ 326-power-of-three/power_of_three_test.go
@@ -0,0 +1,29 @@
+package solution
+
+import (
+	"testing"
+)
+
+func TestIsPowerOfThree(t *testing.T) {
+	cases := []struct {
+		in   int
+		want bool
+	}{
+		{0, false},
+		{1, true},
+		{2, false},
+		{3, true},
+		{4, false},
+		{177147, true},
+	}
+	for _, c := range cases {
+		if got := isPowerOfThree(c.in); got != c.want {
+			t.Errorf(
+				"\n input: %d \n got: %t \n want: %t",
+				c.in,
+				got,
+				c.want,
+			)
+		}
+	}
+}
