e145eecd7fde5fdf06c8085d00f10cb7da7f04c0
Zhang Zhi <fytriht@gmail.com>
2018-04-29 14:19:21 +0800

    Add solutions

--- 326-power-of-three/power_of_three.go
+++ 326-power-of-three/power_of_three.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func isPowerOfThree(n int) bool {
 	for c := 1; c <= n; c *= 3 {
 		if c == n {
@@ -7,4 +11,18 @@ func isPowerOfThree(n int) bool {
 		}
 	}
 	return false
+}
+
+//
+// solution 2
+//
+
+func isPowerOfThree2(n int) bool {
+	if n == 1 {
+		return true
+	}
+	if n == 0 || n%3 != 0 {
+		return false
+	}
+	return isPowerOfThree2(n / 3)
 }
