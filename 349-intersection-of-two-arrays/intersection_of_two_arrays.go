38c552510ac4ee7aaf49b550c509ab987e0a21e1
Zhang Zhi <fytriht@gmail.com>
2018-04-30 13:49:01 +0800

    Enhancements

--- 349-intersection-of-two-arrays/intersection_of_two_arrays.go
+++ 349-intersection-of-two-arrays/intersection_of_two_arrays.go
@@ -1,6 +1,9 @@
 package solution
 
 func intersection(nums1 []int, nums2 []int) []int {
+	if len(nums1) > len(nums2) {
+		return intersection(nums2, nums1)
+	}
 	m := make(map[int]bool, len(nums1))
 	for _, n := range nums1 {
 		m[n] = true
