e6eeeb6b2f219a147c37e66beb74ef12209ee658
Zhang Zhi <fytriht@gmail.com>
2018-04-30 14:06:39 +0800

    Add solutions

--- 349-intersection-of-two-arrays/intersection_of_two_arrays.go
+++ 349-intersection-of-two-arrays/intersection_of_two_arrays.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+	"sort"
+)
+
+//
+// solution 1
+//
+
 func intersection(nums1 []int, nums2 []int) []int {
 	if len(nums1) > len(nums2) {
 		return intersection(nums2, nums1)
@@ -13,6 +21,36 @@ func intersection(nums1 []int, nums2 []int) []int {
 		if m[n] {
 			delete(m, n)
 			ret = append(ret, n)
+		}
+	}
+	return ret
+}
+
+//
+// solution 2
+//
+
+func intersection2(nums1, nums2 []int) []int {
+	sort.Slice(nums1, func(i, j int) bool {
+		return nums1[i] < nums1[j]
+	})
+	sort.Slice(nums2, func(i, j int) bool {
+		return nums2[i] < nums2[j]
+	})
+	var ret []int
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		switch {
+		case nums1[i] < nums2[j]:
+			i++
+		case nums1[i] > nums2[j]:
+			j++
+		default:
+			if len(ret) == 0 || ret[len(ret)-1] != nums1[i] {
+				ret = append(ret, nums1[i])
+			}
+			i++
+			j++
 		}
 	}
 	return ret
