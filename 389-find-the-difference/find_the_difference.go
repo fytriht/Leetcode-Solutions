7b94648b1ea9103d4d4ef661f07e370286a439c3
Zhang Zhi <fytriht@gmail.com>
2018-05-01 10:53:30 +0800

    Add solutions

--- 389-find-the-difference/find_the_difference.go
+++ 389-find-the-difference/find_the_difference.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func findTheDifference(s string, t string) byte {
 	var ret rune
 	for _, r := range t {
@@ -7,6 +11,21 @@ func findTheDifference(s string, t string) byte {
 	}
 	for _, r := range s {
 		ret -= r
+	}
+	return byte(ret)
+}
+
+//
+// solution 2
+//
+
+func findTheDifference2(s string, t string) byte {
+	var ret rune
+	for _, r := range t {
+		ret ^= r
+	}
+	for _, r := range s {
+		ret ^= r
 	}
 	return byte(ret)
 }
