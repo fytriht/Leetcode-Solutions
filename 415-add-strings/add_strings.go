ea337cc25277cda7bf363bb4a22597e83b39aeb6
Zhang Zhi <fytriht@gmail.com>
2018-05-09 22:32:49 +0800

    Simplify

--- 415-add-strings/add_strings.go
+++ 415-add-strings/add_strings.go
@@ -5,27 +5,19 @@ import (
 )
 
 func addStrings(num1 string, num2 string) string {
-	if len(num1) > len(num2) {
-		return addStrings(num2, num1)
-	}
 	var ret string
-	var carry int
-	for i, j := len(num1)-1, len(num2)-1; j >= 0; j-- {
-		s := int(num2[j]-'0') + carry
+	i, j, carry := len(num1)-1, len(num2)-1, 0
+	for i >= 0 || j >= 0 || carry != 0 {
 		if i >= 0 {
-			s += int(num1[i] - '0')
+			carry += int(num1[i] - '0')
 			i--
 		}
-		if s >= 10 {
-			carry = 1
-			s -= 10
-		} else {
-			carry = 0
+		if j >= 0 {
+			carry += int(num2[j] - '0')
+			j--
 		}
-		ret = strconv.Itoa(s) + ret
-	}
-	if carry == 1 {
-		return "1" + ret
+		ret = strconv.Itoa(carry%10) + ret
+		carry /= 10
 	}
 	return ret
 }
