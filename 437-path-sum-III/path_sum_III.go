dc3bd46131dd633406a56a70a0857baf4b6ad269
Zhang Zhi <fytriht@gmail.com>
2018-05-10 23:09:24 +0800

    Add solutions

--- 437-path-sum-III/path_sum_III.go
+++ 437-path-sum-III/path_sum_III.go
@@ -1,12 +1,43 @@
 package solution
 
+import (
+// "fmt"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+//
+// solution 1
+//
+
 func pathSum(root *TreeNode, sum int) int {
+	m := map[int]int{0: 1}
+
+	var rec func(root *TreeNode, target int, acc int) int
+	rec = func(root *TreeNode, target int, acc int) int {
+		if root == nil {
+			return 0
+		}
+		acc += root.Val
+		ret := m[acc-target]
+		m[acc]++
+		ret += rec(root.Left, target, acc) + rec(root.Right, target, acc)
+		m[acc]--
+		return ret
+	}
+
+	return rec(root, sum, 0)
+}
+
+//
+// solution 2
+//
+
+func pathSum2(root *TreeNode, sum int) int {
 	if root == nil {
 		return 0
 	}
