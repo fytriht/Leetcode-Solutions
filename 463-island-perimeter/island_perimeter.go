a71cfa0e69ed89741451d54b38f2ed416e118e1e
Zhang Zhi <fytriht@gmail.com>
2018-05-22 23:34:29 +0800

    Add solutions

--- 463-island-perimeter/island_perimeter.go
+++ 463-island-perimeter/island_perimeter.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func islandPerimeter(grid [][]int) int {
 	var ret int
 	for i := 0; i < len(grid); i++ {
@@ -18,6 +22,29 @@ func islandPerimeter(grid [][]int) int {
 			}
 			if j == 0 || grid[i][j-1] == 0 {
 				ret++
+			}
+		}
+	}
+	return ret
+}
+
+//
+// solution 2
+//
+
+func islandPerimeter2(grid [][]int) int {
+	var ret int
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			ret += 4
+			if i != 0 && grid[i-1][j] == 1 {
+				ret -= 2
+			}
+			if j != 0 && grid[i][j-1] == 1 {
+				ret -= 2
 			}
 		}
 	}
