f4061e0da295bce61374e1bcdc60e4ded4113b71
didi <fytriht@gmail.com>
2018-05-27 19:15:56 +0800

    Add solutions

--- 475-heaters/heaters.go
+++ 475-heaters/heaters.go
@@ -7,6 +7,10 @@ import (
 // TODO: gofmt
 // TODO: test
 
+// 
+// solution 1
+// 
+
 func findRadius(houses []int, heaters []int) int {
 	sort.Ints(houses)
 	sort.Ints(heaters)
@@ -20,6 +24,35 @@ func findRadius(houses []int, heaters []int) int {
 	return ret
 }
 
+//
+// solution 2
+// 
+
+func findRadius(houses []int, heaters []int) int {
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	var ret int
+	for _, h := range houses {
+		i := sort.Search(len(heaters), func (i int) bool {
+			return heaters[i] >= h 
+		})
+        	r := math.MaxInt32
+        	if i == len(heaters) {
+	            r =  h - heaters[i-1]
+     	   } else if i == 0 {
+     	       r = heaters[i] - h
+     	   } else {
+      	      r = min(heaters[i] - h, h - heaters[i-1])
+        	}
+		ret = max(ret, r) 
+	}
+	return ret
+}
+
+//
+// util
+//
+
 func abs(x int) int {
 	if x < 0 {
 		return -x
@@ -29,6 +62,13 @@ func abs(x int) int {
 
 func max(x, y int) int {
 	if x < y {
+		return y
+	}
+	return x
+}
+
+func min(x, y int) int {
+	if x > y {
 		return y
 	}
 	return x
