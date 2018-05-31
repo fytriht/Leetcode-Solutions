6a774ede7d264a7fb7d479cb54cd32d08e7810c1
didi <fytriht@gmail.com>
2018-05-31 23:34:58 +0800

    Refactor

--- 492-construct-the-rectangle/construct_the_rectangle.go
+++ 492-construct-the-rectangle/construct_the_rectangle.go
@@ -4,16 +4,9 @@ package solution
 // TODO: test
 
 func constructRectangle(area int) []int {
-    s := int(math.Sqrt(float64(area)))
-    ret := []int{s, s}
-    for {
-        if a := ret[0] * ret[1]; a < area {
-            ret[0]++
-        } else if a > area {
-            ret[1]--
-        } else {
-            break
-        }
+    w := int(math.Sqrt(float64(area)))
+    for area % w != 0 {
+		w--
     }
-    return ret
+    return []int{area/w, w}
 }
