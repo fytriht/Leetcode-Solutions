b8953631c84fe5bdf34022152178e5d0dddf1f2a
didi <fytriht@gmail.com>
2018-06-04 23:53:57 +0800

    Add solutions

---
+++ 504-base-7/base_7.go
@@ -0,0 +1,11 @@
+package solution
+
+func convertToBase7(num int) string {
+    if num < 0 {
+        return "-" + convertToBase7(-num)
+    }
+    if num < 7 {
+        return strconv.Itoa(num)
+    }
+    return convertToBase7(num / 7) + strconv.Itoa(num % 7)
+}
