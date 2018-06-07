409b8c72ee253fae57914f3c29efbae3e7c81677
Zhang Zhi <fytriht@gmail.com>
2018-06-08 00:10:38 +0800

    Simplify

--- 520-detect-capital/detect_capital.go
+++ 520-detect-capital/detect_capital.go
@@ -1,35 +1,15 @@
 package solution
 
 func detectCapitalUse(word string) bool {
-	return isAllUpper(word) || isAllLower(word) || isOnlyFirstLetterUpper(word)
-}
-
-func isOnlyFirstLetterUpper(s string) bool {
-	return isUpper(rune(s[0])) && isAllLower(s[1:])
-}
-
-func isAllUpper(s string) bool {
-	for _, r := range s {
-		if isLower(r) {
-			return false
-		}
-	}
-	return true
-}
-
-func isAllLower(s string) bool {
-	for _, r := range s {
+	var cnt int
+	for _, r := range word {
 		if isUpper(r) {
-			return false
+			cnt++
 		}
 	}
-	return true
+	return cnt == 0 || cnt == len(word) || cnt == 1 && isUpper(rune(word[0]))
 }
 
 func isUpper(r rune) bool {
 	return r >= 'A' && r <= 'Z'
-}
-
-func isLower(r rune) bool {
-	return !isUpper(r)
 }
