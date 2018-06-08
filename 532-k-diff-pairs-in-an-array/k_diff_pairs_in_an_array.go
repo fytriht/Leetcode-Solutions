57bd6fc2b398644336d6196874bd4ca8335f5401
Zhang Zhi <fytriht@gmail.com>
2018-06-08 23:07:28 +0800

    Simplify

--- 532-k-diff-pairs-in-an-array/k_diff_pairs_in_an_array.go
+++ 532-k-diff-pairs-in-an-array/k_diff_pairs_in_an_array.go
@@ -1,40 +1,17 @@
 package solution
 
-import (
-	"sort"
-)
-
 func findPairs(nums []int, k int) int {
 	var ret int
 
-	sort.Ints(nums)
-	for i := 0; i < len(nums)-1; i++ {
-		if k == 0 && nums[i+1]-nums[i] == 0 {
-			ret++
-		}
-		for nums[i+1]-nums[i] == 0 {
-			i++
-			if i+1 == len(nums) {
-				return ret
-			}
-		}
-
-		if diff := nums[i+1] - nums[i]; diff >= k {
-			if diff == k {
-				ret++
-			}
-			continue
-		}
-
-		for j := i + 2; j < len(nums); j++ {
-			if diff := nums[j] - nums[i]; diff >= k {
-				if diff == k {
-					ret++
-				}
-				break
-			}
-		}
+	m := make(map[int]int)
+	for _, n := range nums {
+		m[n]++
 	}
 
+	for key, count := range m {
+		if k == 0 && count > 1 || k > 0 && m[key+k] > 0 {
+			ret++
+		}
+	}
 	return ret
 }
