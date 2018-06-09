e300235249ca424f1fcba105b9761479f3b6da4a
Zhang Zhi <fytriht@gmail.com>
2018-06-09 22:36:36 +0800

    Add solutions

--- 541-reverse-string-II/reverse_string_II.go
+++ 541-reverse-string-II/reverse_string_II.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func reverseStr(s string, k int) string {
 	if k >= len(s) {
 		return reverse(s)
@@ -16,4 +20,25 @@ func reverse(s string) string {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
+}
+
+//
+// solution 2
+//
+
+func reverseStr2(s string, k int) string {
+	r := []rune(s)
+	for i := 0; i < len(r); i += 2 * k {
+		for left, right := i, min(i+k-1, len(r)-1); left < right; left, right = left+1, right-1 {
+			r[left], r[right] = r[right], r[left]
+		}
+	}
+	return string(r)
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
 }
