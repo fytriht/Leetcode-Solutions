231322f3869949462cbb32bccf5ae7588bc64f5e
Zhang Zhi <fytriht@gmail.com>
2018-06-13 21:53:37 +0800

    Add solutions

---
+++ 599-minimum-index-sum-of-two-lists/minimum_index_sum_of_two_lists.go
@@ -0,0 +1,26 @@
+package solution
+
+import (
+	"math"
+)
+
+func findRestaurant(list1, list2 []string) []string {
+	m := map[string]int{}
+	for i, s := range list2 {
+		m[s] = i
+	}
+
+	var ret []string
+	idxSum := math.MaxInt32
+	for i, s := range list1 {
+		if _, ok := m[s]; !ok {
+			continue
+		}
+		if sum := i + m[s]; len(ret) == 0 || sum <= idxSum {
+			ret = append(ret, s)
+			idxSum = sum
+		}
+
+	}
+	return ret
+}
