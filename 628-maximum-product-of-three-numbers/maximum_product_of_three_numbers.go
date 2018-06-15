98778e2c005c46c1d8cd985c934033df61d8b89c
Zhang Zhi <fytriht@gmail.com>
2018-06-15 21:39:40 +0800

    Add solutions

--- 628-maximum-product-of-three-numbers/maximum_product_of_three_numbers.go
+++ 628-maximum-product-of-three-numbers/maximum_product_of_three_numbers.go
@@ -1,8 +1,13 @@
 package solution
 
 import (
+	"math"
 	"sort"
 )
+
+//
+// solution 1
+//
 
 func maximumProduct(nums []int) int {
 	sort.Ints(nums)
@@ -11,4 +16,35 @@ func maximumProduct(nums []int) int {
 		return neg
 	}
 	return ret
+}
+
+//
+// solution 2
+//
+
+func maximumProduct2(nums []int) int {
+	min1, min2 := math.MaxInt32, math.MaxInt32
+	max1, max2, max3 := math.MinInt32, math.MinInt32, math.MinInt32
+	for _, n := range nums {
+		if n < min1 {
+			min1, min2 = n, min1
+		} else if n < min2 {
+			min2 = n
+		}
+		if n > max1 {
+			max1, max2, max3 = n, max1, max2
+		} else if n > max2 {
+			max2, max3 = n, max2
+		} else if n > max3 {
+			max3 = n
+		}
+	}
+	return max(min1*min2*max1, max1*max2*max3)
+}
+
+func max(x, y int) int {
+	if y > x {
+		return y
+	}
+	return x
 }
