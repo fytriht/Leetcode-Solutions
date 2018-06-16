2f8cdec84a1f0e58da7e17025503ac641ffd220b
Zhang Zhi <fytriht@gmail.com>
2018-06-16 17:31:57 +0800

    Refactor

--- 643-maximum-average-subarray-i/maximum_average_subarray_i.go
+++ 643-maximum-average-subarray-i/maximum_average_subarray_i.go
@@ -2,9 +2,9 @@ package solution
 
 func findMaxAverage(nums []int, k int) float64 {
 	var ret, sum int
-	for i := 0; i < k; i++ {
-		sum += nums[i]
-		ret += nums[i]
+	for _, n := range nums[:k] {
+		sum += n
+		ret += n
 	}
 	for i := 1; i < len(nums)-k+1; i++ {
 		sum -= nums[i-1] - nums[i+k-1]
