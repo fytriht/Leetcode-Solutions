f97db71a4238b41b0b7f422bc3570b903f0888c4
Zhang Zhi <fytriht@gmail.com>
2018-06-18 18:42:18 +0800

    Add solutions

--- 657-judge-route-circle/judge_route_circle.go
+++ 657-judge-route-circle/judge_route_circle.go
@@ -1,9 +1,34 @@
 package solution
 
+//
+// solution 1
+//
+
 func judgeCircle(moves string) bool {
 	m := make(map[rune]int, 4)
 	for _, r := range moves {
 		m[r]++
 	}
 	return m['R'] == m['L'] && m['U'] == m['D']
+}
+
+//
+// solution 2
+//
+
+func judgeCircle2(moves string) bool {
+	var x, y int
+	for _, r := range moves {
+		switch r {
+		case 'R':
+			x++
+		case 'L':
+			x--
+		case 'U':
+			y++
+		case 'D':
+			y--
+		}
+	}
+	return x == 0 && y == 0
 }
