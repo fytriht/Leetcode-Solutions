b0ea2af1166e9ef96ad28064f477390874c20919
Zhang Zhi <fytriht@gmail.com>
2018-06-19 23:47:25 +0800

    Refactor

--- non_decreasing_array.go
+++ non_decreasing_array.go
@@ -4,10 +4,7 @@ func checkPossibility(nums []int) bool {
 	var modified bool
 	for i := 0; i+1 < len(nums); i++ {
 		if nums[i] > nums[i+1] {
-			if modified {
-				return false
-			}
-			if i > 0 && i+2 < len(nums) && nums[i+1] < nums[i-1] && nums[i+2] < nums[i] {
+			if modified || i > 0 && i+2 < len(nums) && nums[i+1] < nums[i-1] && nums[i+2] < nums[i] {
 				return false
 			}
 			modified = true
