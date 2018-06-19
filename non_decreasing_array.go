0dada1624d28634334b1809c26a8cbe8101eda90
Zhang Zhi <fytriht@gmail.com>
2018-06-19 23:41:12 +0800

    Add solutions

---
+++ non_decreasing_array.go
@@ -0,0 +1,17 @@
+package solution
+
+func checkPossibility(nums []int) bool {
+	var modified bool
+	for i := 0; i+1 < len(nums); i++ {
+		if nums[i] > nums[i+1] {
+			if modified {
+				return false
+			}
+			if i > 0 && i+2 < len(nums) && nums[i+1] < nums[i-1] && nums[i+2] < nums[i] {
+				return false
+			}
+			modified = true
+		}
+	}
+	return true
+}
