1872bf31d6795737036b04de53a0063cc3da259c
Zhang Zhi <fytriht@gmail.com>
2018-06-23 15:59:36 +0800

    Refactor

--- 680-valid-palindrome-ii/valid_palindrome_ii.go
+++ 680-valid-palindrome-ii/valid_palindrome_ii.go
@@ -5,21 +5,21 @@ package solution
 //
 
 func validPalindrome(s string) bool {
-	var rec func(string, int, int, bool) bool
-	rec = func(s string, start int, end int, deleted bool) bool {
-		if start >= end {
+	var rec func(string, bool) bool
+	rec = func(s string, deleted bool) bool {
+		if len(s) <= 1 {
 			return true
 		}
-		if s[start] == s[end] {
-			return rec(s, start+1, end-1, deleted)
+		if s[0] == s[len(s)-1] {
+			return rec(s[1:len(s)-1], deleted)
 		}
 		if deleted {
 			return false
 		}
-		return rec(s, start, end-1, true) || rec(s, start+1, end, true)
+		return rec(s[1:len(s)], true) || rec(s[0:len(s)-1], true)
 
 	}
-	return rec(s, 0, len(s)-1, false)
+	return rec(s, false)
 }
 
 //
