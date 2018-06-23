ec6824fbd6bc20bb0ed279255cda00d2de915462
Zhang Zhi <fytriht@gmail.com>
2018-06-23 15:53:38 +0800

    Add solutions

--- 680-valid-palindrome-ii/valid_palindrome_ii.go
+++ 680-valid-palindrome-ii/valid_palindrome_ii.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func validPalindrome(s string) bool {
 	var rec func(string, int, int, bool) bool
 	rec = func(s string, start int, end int, deleted bool) bool {
@@ -16,4 +20,26 @@ func validPalindrome(s string) bool {
 
 	}
 	return rec(s, 0, len(s)-1, false)
+}
+
+//
+// solution 2
+//
+
+func validPalindrome2(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return isValid(s[i+1:j+1]) || isValid(s[i:j])
+		}
+	}
+	return true
+}
+
+func isValid(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
 }
