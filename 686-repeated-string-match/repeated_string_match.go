6377851af88336e33a7af1f7657b625b093020df
Zhang Zhi <fytriht@gmail.com>
2018-06-24 17:21:07 +0800

    Add solutions

--- 686-repeated-string-match/repeated_string_match.go
+++ 686-repeated-string-match/repeated_string_match.go
@@ -19,3 +19,13 @@ func repeatedStringMatch(A string, B string) int {
 	}
 	return -1
 }
+
+func repeatedStringMatch2(A string, B string) int {
+	for s, i := A, 1; i <= len(B)/len(A)+2; i++ {
+		if strings.Contains(s, B) {
+			return i
+		}
+		s += A
+	}
+	return -1
+}
