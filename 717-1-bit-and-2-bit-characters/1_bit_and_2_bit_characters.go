6635b337682f8d8ffd49f046d15c3890de3d52ec
Zhang Zhi <fytriht@gmail.com>
2018-07-01 10:54:04 +0800

    Simplify

--- 717-1-bit-and-2-bit-characters/1_bit_and_2_bit_characters.go
+++ 717-1-bit-and-2-bit-characters/1_bit_and_2_bit_characters.go
@@ -1,15 +1,9 @@
 package solution
 
 func isOneBitCharacter(bits []int) bool {
-	return len(bits) == 1 || bits[len(bits)-2] == 0 || !canConstruct(bits[:len(bits)-2])
-}
-
-func canConstruct(bits []int) bool {
-	if len(bits) == 0 {
-		return true
+	var i int
+	for i < len(bits)-1 {
+		i += bits[i] + 1
 	}
-	if len(bits) == 1 {
-		return bits[0] == 0
-	}
-	return bits[len(bits)-1] == 0 && canConstruct(bits[:len(bits)-1]) || bits[len(bits)-2] == 1 && canConstruct(bits[:len(bits)-2])
+	return i == len(bits)-1
 }
