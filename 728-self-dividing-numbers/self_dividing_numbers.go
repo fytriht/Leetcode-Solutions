d2246ee28d498a0954908b2eaa636ca75f49b009
Zhang Zhi <fytriht@gmail.com>
2018-07-03 23:57:54 +0800

    Add solutions

---
+++ 728-self-dividing-numbers/self_dividing_numbers.go
@@ -0,0 +1,21 @@
+package solution
+
+func selfDividingNumbers(left int, right int) []int {
+	ret := []int{}
+	for left <= right {
+		if isSelfDividing(left) {
+			ret = append(ret, left)
+		}
+		left++
+	}
+	return ret
+}
+
+func isSelfDividing(n int) bool {
+	for m := n; n != 0; n /= 10 {
+		if mod := n % 10; mod == 0 || m%mod != 0 {
+			return false
+		}
+	}
+	return true
+}
