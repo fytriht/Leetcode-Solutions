c63072e79b8bfbb32846c15154eb3aa5ab3de0e6
Zhang Zhi <fytriht@gmail.com>
2018-07-06 23:15:51 +0800

    Add solutions

--- 746-min-cost-climbing-stairs/min_cost_climbing_stairs.go
+++ 746-min-cost-climbing-stairs/min_cost_climbing_stairs.go
@@ -1,11 +1,28 @@
 package solution
 
+//
+// solution 1
+//
+
 func minCostClimbingStairs(cost []int) int {
 	dp := make([]int, len(cost)+1)
 	for i := 2; i < len(cost)+1; i++ {
 		dp[i] = min(dp[i-2]+cost[i-2], dp[i-1]+cost[i-1])
 	}
 	return dp[len(dp)-1]
+}
+
+//
+// solution 2
+//
+
+func minCostClimbingStairs2(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0], dp[1] = cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+	}
+	return min(dp[len(dp)-1], dp[len(dp)-2])
 }
 
 func min(x, y int) int {
