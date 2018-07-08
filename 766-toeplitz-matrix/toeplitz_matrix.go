a75aa29c28c1535ae55927d52de0bbcaaea54d6e
Zhang Zhi <fytriht@gmail.com>
2018-07-08 08:32:27 +0800

    Add solutions

--- 766-toeplitz-matrix/toeplitz_matrix.go
+++ 766-toeplitz-matrix/toeplitz_matrix.go
@@ -1,6 +1,25 @@
 package solution
 
+//
+// solution 1
+//
+
 func isToeplitzMatrix(matrix [][]int) bool {
+	for i := 0; i < len(matrix)-1; i++ {
+		for j := 0; j < len(matrix[i])-1; j++ {
+			if matrix[i][j] != matrix[i+1][j+1] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+//
+// solution 2
+//
+
+func isToeplitzMatrix2(matrix [][]int) bool {
 	for i := 0; i < len(matrix)-1; i++ {
 		currR, nextR := matrix[i], matrix[i+1]
 		if !isShallowEq(currR[:len(currR)-1], nextR[1:]) {
