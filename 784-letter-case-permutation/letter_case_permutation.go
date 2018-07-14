8a142fdfa3452f19ee12d869ddc0a3035bd2a604
Zhang Zhi <fytriht@gmail.com>
2018-07-14 09:14:53 +0800

    Add solutions

--- 784-letter-case-permutation/letter_case_permutation.go
+++ 784-letter-case-permutation/letter_case_permutation.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+	"unicode"
+)
+
+//
+// solution 1
+//
+
 func letterCasePermutation(S string) []string {
 	ret := []string{""}
 	for _, b := range S {
@@ -14,6 +22,39 @@ func letterCasePermutation(S string) []string {
 				ret[i] += string(b)
 			}
 		}
+	}
+	return ret
+}
+
+//
+// solution 2
+//
+
+func letterCasePermutation2(S string) []string {
+	var letterCnt uint32
+	for _, b := range S {
+		if b > '9' {
+			letterCnt++
+		}
+	}
+
+	var ret []string
+	for i := 0; i < 1<<letterCnt; i++ {
+		var j uint32
+		var s string
+		for _, b := range S {
+			if b > '9' {
+				if (i>>j)&1 == 1 {
+					s += string(unicode.ToLower(b))
+				} else {
+					s += string(unicode.ToUpper(b))
+				}
+				j++
+			} else {
+				s += string(b)
+			}
+		}
+		ret = append(ret, s)
 	}
 	return ret
 }
