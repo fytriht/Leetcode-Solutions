8635702d85b53bd8467ae640ed50aa2a48cdd8d0
Zhang Zhi <fytriht@gmail.com>
2018-07-14 09:03:10 +0800

    Refactor

--- 784-letter-case-permutation/letter_case_permutation.go
+++ 784-letter-case-permutation/letter_case_permutation.go
@@ -3,16 +3,16 @@ package solution
 func letterCasePermutation(S string) []string {
 	ret := []string{""}
 	for _, b := range S {
-		if b >= '0' && b <= '9' {
-			for i := range ret {
-				ret[i] += string(b)
-			}
-		} else {
+		if b > '9' {
 			next := []string{}
 			for _, s := range ret {
 				next = append(next, s+string(b), s+string(b^32))
 			}
 			ret = next
+		} else {
+			for i := range ret {
+				ret[i] += string(b)
+			}
 		}
 	}
 	return ret
