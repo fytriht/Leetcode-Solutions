d9842cecfb051621bb5ea2b0802c597e299b9fc8
Zhang Zhi <fytriht@gmail.com>
2018-07-19 22:58:33 +0800

    Refactor

--- 796-rotate-string/rotate_string.go
+++ 796-rotate-string/rotate_string.go
@@ -1,14 +1,11 @@
 package solution
 
 func rotateString(A string, B string) bool {
-	if A == B {
-		return true
-	}
 	for i := 0; i < len(A)-1; i++ {
 		A = A[1:] + string(A[0])
 		if A == B {
 			return true
 		}
 	}
-	return false
+	return A == B
 }
