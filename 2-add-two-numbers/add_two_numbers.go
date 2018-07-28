9a31da3f0252a3dbadfee540b150cb0d64b18691
Zhang Zhi <fytriht@gmail.com>
2018-07-28 08:13:22 +0800

    Refactor

--- 2-add-two-numbers/add_two_numbers.go
+++ 2-add-two-numbers/add_two_numbers.go
@@ -7,30 +7,23 @@ type ListNode struct {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	ret := &ListNode{}
-	head := ret
+	curr := ret
 	carry := 0
 
-	for l1 != nil || l2 != nil {
-		s := carry
+	for l1 != nil || l2 != nil || carry != 0 {
+		val := carry
 		if l1 != nil {
-			s += l1.Val
+			val += l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
-			s += l2.Val
+			val += l2.Val
 			l2 = l2.Next
 		}
-		head.Next = &ListNode{
-			Val: s % 10,
-		}
-		head = head.Next
-		carry = s / 10
+		curr.Next = &ListNode{Val: val % 10}
+		curr = curr.Next
+		carry = val / 10
 	}
 
-	if carry != 0 {
-		head.Next = &ListNode{
-			Val: carry,
-		}
-	}
 	return ret.Next
 }
