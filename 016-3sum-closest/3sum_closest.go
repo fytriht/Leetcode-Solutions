ad2d314a3367dfc423b7e858eb3b6a0dedabe14e
Zhang Zhi <fytriht@gmail.com>
2018-08-05 11:21:03 +0800

    Refactor

--- 016-3sum-closest/3sum_closest.go
+++ 016-3sum-closest/3sum_closest.go
@@ -11,15 +11,15 @@ func threeSumClosest(nums []int, target int) int {
 	for i := 0; i < len(nums)-2; i++ {
 		for j, k := i+1, len(nums)-1; j < k; {
 			sum := nums[i] + nums[j] + nums[k]
-			if diff := abs(sum - target); diff < minDiff {
-				ret, minDiff = sum, diff
-			}
 			if sum > target {
 				k--
 			} else if sum < target {
 				j++
 			} else {
 				return sum
+			}
+			if diff := abs(sum - target); diff < minDiff {
+				ret, minDiff = sum, diff
 			}
 		}
 	}
