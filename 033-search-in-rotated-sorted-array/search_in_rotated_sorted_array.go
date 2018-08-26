3ad771543cf2da6d95f518d6433920c0061df5e4
Zhang Zhi <fytriht@gmail.com>
2018-08-26 20:01:16 +0800

    Refactor

--- 033-search-in-rotated-sorted-array/search_in_rotated_sorted_array.go
+++ 033-search-in-rotated-sorted-array/search_in_rotated_sorted_array.go
@@ -7,18 +7,11 @@ func search(nums []int, target int) int {
 		if nums[mid] == target {
 			return mid
 		}
-		if nums[mid] < nums[right] {
-			if target > nums[mid] && target <= nums[right] {
-				left = mid + 1
-			} else {
-				right = mid - 1
-			}
+		if nums[mid] < nums[right] && target > nums[mid] && target <= nums[right] ||
+			nums[mid] >= nums[right] && (target < nums[left] || target >= nums[mid]) {
+			left = mid + 1
 		} else {
-			if target >= nums[left] && target < nums[mid] {
-				right = mid - 1
-			} else {
-				left = mid + 1
-			}
+			right = mid - 1
 		}
 	}
 	return -1
