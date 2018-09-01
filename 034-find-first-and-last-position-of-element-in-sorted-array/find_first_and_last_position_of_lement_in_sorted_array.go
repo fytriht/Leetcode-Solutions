c162ec9c654364f6b91af613ef9d422e69822e64
Zhang Zhi <fytriht@gmail.com>
2018-09-01 23:17:07 +0800

    Add solutions

--- 034-find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_lement_in_sorted_array.go
+++ 034-find-first-and-last-position-of-element-in-sorted-array/find_first_and_last_position_of_lement_in_sorted_array.go
@@ -1,5 +1,9 @@
 package solution
 
+//
+// solution 1
+//
+
 func searchRange(nums []int, target int) []int {
 	pos := -1
 	for left, right := 0, len(nums)-1; left <= right; {
@@ -24,4 +28,34 @@ func searchRange(nums []int, target int) []int {
 		right++
 	}
 	return []int{left, right}
+}
+
+//
+// solution 2
+//
+
+func searchRange2(nums []int, target int) []int {
+	ret := []int{-1, -1}
+	left, right := 0, len(nums)-1
+	for left < right {
+		if mid := left + (right-left)/2; nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	if right < 0 || nums[right] != target {
+		return ret
+	}
+	ret[0] = right
+	right = len(nums)
+	for left < right {
+		if mid := left + (right-left)/2; nums[mid] <= target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	ret[1] = left - 1
+	return ret
 }
