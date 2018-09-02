274881ae2d5b8b5192dd5405ea03df72d322307d
Zhang Zhi <fytriht@gmail.com>
2018-09-02 09:47:27 +0800

    Refactor

--- 036-valid-sudoku/valid_sudoku.go
+++ 036-valid-sudoku/valid_sudoku.go
@@ -2,31 +2,31 @@ package solution
 
 func isValidSudoku(board [][]byte) bool {
 	for i := 0; i < 9; i++ {
-		rm := make(map[byte]bool)
-		cm := make(map[byte]bool)
-		for j := 0; j < 9; j++ {
-			rb, cb := board[i][j], board[j][i]
-			if rm[rb] || cm[cb] {
-				return false
-			}
-			rm[rb], cm[cb] = rb != '.', cb != '.'
+		if hasRepetition(board, i, i+1, 0, 9) || hasRepetition(board, 0, 9, i, i+1) {
+			return false
 		}
 	}
 	for i := 0; i < 9; i += 3 {
 		for j := 0; j < 9; j += 3 {
-			m := make(map[byte]bool)
-			for k := 0; k < 3; k++ {
-				for n := 0; n < 3; n++ {
-					if b := board[i+k][j+n]; b != '.' {
-						if m[b] {
-							return false
-						}
-						m[b] = true
-					}
-				}
-
+			if hasRepetition(board, i, i+3, j, j+3) {
+				return false
 			}
 		}
 	}
 	return true
+}
+
+func hasRepetition(board [][]byte, ir, jr, ic, jc int) bool {
+	m := make(map[byte]bool)
+	for i := ir; i < jr; i++ {
+		for j := ic; j < jc; j++ {
+			if b := board[i][j]; b != '.' {
+				if m[b] {
+					return true
+				}
+				m[b] = true
+			}
+		}
+	}
+	return false
 }
